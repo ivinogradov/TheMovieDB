b0568fc6bb14e2e71b3bfed0dd49cb3a2caf2b83
ivinogradov <ilya.vngrdv@gmail.com>
2024-04-15 23:50:38 -0500

    Implemented MovieService

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ApiResult, MovieResult } from './interfaces';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = environment.apiKey;
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
+  private http = inject(HttpClient);
   constructor() { }
+
+  getTopRatedMovies(page = 1): Observable<ApiResult> {
+    return this.http.get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`)
+  }
+
+  getMovieDetails(movieId: string): Observable<MovieResult> {
+    return this.http.get<MovieResult>(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+  }
 }
