cae82106f6504d4f4f4a65e9d2f27baa232b0397
Ilya Vinogradov <ilya.vngrdv@gmail.com>
2024-04-21 23:22:34 -0500

    Implemented "details" page

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, WritableSignal, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonBackButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonText, IonCardContent, IonLabel, IonImg, IonItem, IonIcon } from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+import { MovieResult } from '../services/interfaces';
+import { addIcons } from 'ionicons';
+import { cashOutline, calendarOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonItem, IonImg, IonLabel, IonCardContent, IonText, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonBackButton, IonButtons, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage {
+  private movieService = inject(MovieService);
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+  public movie: WritableSignal<MovieResult | null> = signal(null);
 
-  constructor() { }
+  @Input()
+  set id(movieId: string) {
+    this.movieService.getMovieDetails(movieId).subscribe((movie) => {
+      console.log(movie);
 
-  ngOnInit() {
+      this.movie.set(movie);
+    });
   }
+
+  constructor() {
+    addIcons({ cashOutline, calendarOutline });
+   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { RouteReuseStrategy, provideRouter, withComponentInputBinding } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
@@ -17,6 +17,6 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideHttpClient(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
   ],
 });
